f2acbf5dea425e4cf74cb4b6184771f9f08d4eb7
zachjbrowning <zachjbrowning@outlook.com>
2020-05-24 17:00:58 +1000

    styled

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import ZJBlogo from './ZJBlogo'
 
 export default function Base() {
     return (
         <div className="base-frame">
-            Welcome to React!!
+            <div className="title-frame">
+                <strong>Happy Building!!</strong>
+            </div>
+            <div className="logo-holder">
+                <ZJBlogo/>
+            </div>
         </div>
     )
 }
